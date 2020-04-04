7ef28f804a0441699a39e96fef5067568816e199
imosonyi <mosonyi15p@yahoo.com>
2020-04-04 20:55:47 +0200

    Added dockerfile to angular project

--- bvtech-front/e2e/src/app.e2e-spec.ts
+++ bvtech-front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to bvtech-front!');
+    expect(page.getTitleText()).toEqual('Welcome to bvtech-angular!');
   });
 
   afterEach(async () => {
--- bvtech-front/karma.conf.js
+++ bvtech-front/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/bvtech-front'),
+      dir: require('path').join(__dirname, './coverage/bvtech-angular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
