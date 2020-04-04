cd3fd4efc61320fb3f8e83a665adbc14fac07649
imosonyi <mosonyi15p@yahoo.com>
2020-04-05 01:08:14 +0200

    Added documentation for gateway app

--- bvtech-springboot/bvtech-gateway/src/main/java/hu/imosonyi/bvtech/gateway/BVTechGatewayApplication.java
+++ bvtech-springboot/bvtech-gateway/src/main/java/hu/imosonyi/bvtech/gateway/BVTechGatewayApplication.java
@@ -3,6 +3,11 @@ package hu.imosonyi.bvtech.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot main class.
+ *
+ * @author István Mosonyi
+ */
 @SpringBootApplication
 public class BVTechGatewayApplication {
 
